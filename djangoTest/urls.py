491dc9096115d9fc66ee869ba87d12ec60a4971a
Vitaly <ivanov.arduan@yandex.ru>
2021-11-23 17:51:04 +0300

    Тренируюсь с роутами.

--- djangoTest/urls.py
+++ djangoTest/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path
 
 from test_app.views import index
+from blog.views import index_blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    # path('', index),
+    path('', index_blog)
 ]
